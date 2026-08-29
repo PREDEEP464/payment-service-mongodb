9497368f96a3b3319f85d0154a21e245362e9a0c
Predeepkumar U S <usppcbe@gmail.com>
2026-08-29 14:16:18 +0530

    Cleanup and validation done

--- src/main/java/com/reactive/payment/controller/PaymentController.java
+++ src/main/java/com/reactive/payment/controller/PaymentController.java
@@ -9,8 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -36,16 +36,18 @@ public class PaymentController {
     }
 
     @GetMapping
-    public Mono<ResponseEntity<ApiResponse<Flux<PaymentResponse>>>> getAllPayments() {
+    public Mono<ResponseEntity<ApiResponse<List<PaymentResponse>>>> getAllPayments() {
 
-        return Mono.just(
-                ResponseEntity.ok(
-                        new ApiResponse<>(
-                                "Payments fetched successfully",
-                                paymentService.getAllPayments()
+        return paymentService.getAllPayments()
+                .collectList()
+                .map(payments ->
+                        ResponseEntity.ok(
+                                new ApiResponse<>(
+                                        "Payments fetched successfully",
+                                        payments
+                                )
                         )
-                )
-        );
+                );
     }
 
     @GetMapping("/{id}")
